5944478caa661ea69b6dc5c6cb8893d7a9f741c6
Arjan Gahatraj <arjan.gahatrajsunar@gmail.com>
2021-01-12 14:14:53 +0545

    creating a default frame

---
+++ src/GUI/CreateFrame.java
@@ -0,0 +1,12 @@
+package GUI;
+
+import javax.swing.*;
+
+public class CreateFrame extends JFrame {
+  public CreateFrame(int width,int height){
+    setSize(width,height);
+    setVisible(true);
+    setLayout(null);
+  }
+
+}
